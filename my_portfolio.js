a292982828bf14115b1f0f74c960094a520fe19b
JD Tadlock <jt7903@gmail.com>
2022-05-09 10:29:43 -0400

    added comments

--- my_portfolio.js
+++ my_portfolio.js
@@ -21,16 +21,23 @@ $('.top-row h2').text('something different').css('color', 'red');
 
 $('p').text(`some text`);
 
+// Notice how I didn't use a # - id css selector. Since we only had one button on the page, I just selected it using the css selector "button"
 $('button').click(function () {
   // $(this).css('color', 'purple');
   // $('.my-paragraph').siblings('p').css('background', 'red');
   $('.my-paragraph').parent().css('background', 'red');
 });
 
+// We create functions using the function keyword and a custom name, followed by 
+// opening/closing parens and opening/closing curly braces
+
 // function myFunc(somethingThatWasPassedIn) {
-//   console.log(somethingThatWasPassedIn);
+  // the param or variable above is used to reference the value we passed in when calling the function
+  // console.log(somethingThatWasPassedIn);
 // }
 
+// We call a function using it's name, followed by opening/closing parens
+// We can also pass values into functions when we call them. The values can be used in the function as you see above
 // myFunc('hey, print this string!');
 // myFunc('different');
 // myFunc('new string!');
